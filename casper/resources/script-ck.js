620dc8bde00b049ed13b263134f08f7fe6c29bdc
Adam McCombs <adamjmccombs@gmail.com>
2015-03-16 00:08:09 -0500

    v.03 - Minor updates

--- casper/resources/script-ck.js
+++
@@ -1,2 +0,0 @@
-// Need some javascript? The craft admin already loads jQuery
-$(document).ready(function(){$(".js-contact").on("click",function(e){e.preventDefault();SnappyWidget.open({contact:!0})})});
